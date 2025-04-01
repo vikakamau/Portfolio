c92ce3c93b3cd6aad3023a934a534c5653eb8136
Dickson Murithi <dicksonmurithi155@gmail.com>
2025-04-01 11:32:20 +0300

    changes

--- my-portfolio/src/Components/Navbar.js
+++ my-portfolio/src/Components/Navbar.js
@@ -27,7 +27,7 @@ const closeMenu = () =>{
                 <li><AnchorLink className="anchor-link" offset={50} href='#services'><p onClick={() => setMenu("services")}>Services</p></AnchorLink>{menu === "services" ? <img src={underline} alt='' /> : <></>}</li>
                 <li><AnchorLink className="anchor-link" offset={50} href='#work'><p onClick={() => setMenu("work")}>Portfolio</p></AnchorLink>{menu === "work" ? <img src={underline} alt='' /> : <></>}</li>
                 <li><AnchorLink className="anchor-link" offset={50} href='#contact'><p onClick={() => setMenu("contact")}>Contact</p></AnchorLink>{menu === "contact" ? <img src={underline} alt='' /> : <></>}</li>
-            </ul>
+                </ul>
             <div className='nav-connect'><AnchorLink className="anchor-link" offset={50} href='#contact'>Connect with me</AnchorLink></div>
         </div>
     );
